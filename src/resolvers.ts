4b38bcf0afee1754095c981748a8d07458ae86ff
Kyle Erikson <Kyle.Erikson@intelex.com>
2021-03-16 13:48:27 -0600

    Working on react integration

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,9 +1,41 @@
 import { IResolvers } from "graphql-tools";
+import { PrismaClient } from "@prisma/client";
 
 const resolvers: IResolvers = {
-    Query: {
-        helloWorld: () => "Hello world!"
-    }
+  Query: {
+    getMatrices: async () => {
+      const prisma = new PrismaClient();
+
+      return await prisma.matrix.findMany();
+    },
+    getLevels: async (parent, { matrixId }) => {
+      const prisma = new PrismaClient();
+
+      return await prisma.level.findMany({
+        where: {
+          matrix_id: matrixId,
+        },
+      });
+    },
+    getReports: async (parent, { managerId }) => {
+      const prisma = new PrismaClient();
+
+      return await prisma.user.findMany({
+        where: {
+          manager_id: managerId,
+        },
+      });
+    },
+    getMatrixReportsForUser: async (parent, { userId }) => {
+      const prisma = new PrismaClient();
+
+      return await prisma.matrix_report.findMany({
+        where: {
+          user_id: userId,
+        },
+      });
+    },
+  },
 };
 
 export default resolvers;
