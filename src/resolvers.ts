7a5102c01b42f0e0d3c19c4a6d8921774cd27c80
Kyle Erikson <Kyle.Erikson@intelex.com>
2021-04-15 23:59:49 -0600

    Added some save functionality for new reports

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,6 +1,7 @@
 import { IResolvers } from "graphql-tools";
 import { PrismaClient } from "@prisma/client";
-import { BaseReport } from "./generated/graphql";
+import { BaseReport, Report } from "./generated/graphql";
+import { ReportInput } from "./types/customTypes";
 
 const resolvers: IResolvers = {
   Query: {
@@ -39,9 +40,12 @@ const resolvers: IResolvers = {
     getBaseReportForUser: async (parent, { userId, matrixId }) => {
       const prisma = new PrismaClient();
 
+      let userID = Number(userId);
+      let matrixID = Number(matrixId);
+
       const results = await prisma.user.findUnique({
         where: {
-          id: Number(userId),
+          id: userID,
         },
         select: {
           level_id: true,
@@ -52,7 +56,7 @@ const resolvers: IResolvers = {
 
       const report = await prisma.key_area.findMany({
         where: {
-          matrix_id: Number(matrixId),
+          matrix_id: matrixID,
         },
         include: {
           attribute: {
@@ -72,6 +76,60 @@ const resolvers: IResolvers = {
       });
 
       return report;
+    },
+  },
+  Mutation: {
+    saveReport: async (parent, { input }) => {
+      const {
+        rating_id,
+        matrix_report_id,
+        matrix_id,
+        competency_id,
+        user_id,
+        rating,
+        notes,
+      } = input;
+
+      const prisma = new PrismaClient();
+
+      let matrixReportId = matrix_report_id ? Number(matrix_report_id) : 0;
+      let ratingId = rating_id ? Number(rating_id) : 0;
+
+      let ratingRecord = {};
+
+      if (ratingId > 0) {
+        ratingRecord = await prisma.rating.update({
+          where: {
+            id: ratingId,
+          },
+          data: {
+            rating: Number(rating),
+            notes: notes,
+          },
+        });
+      } else {
+        if (matrixReportId <= 0) {
+          const newReport = await prisma.matrix_report.create({
+            data: {
+              matrix_id: Number(matrix_id),
+              user_id: Number(user_id),
+            },
+          });
+
+          matrixReportId = newReport.id;
+        }
+        ratingRecord = await prisma.rating.create({
+          data: {
+            competency_id: Number(competency_id),
+            user_id: Number(user_id),
+            rating: Number(rating),
+            notes: notes,
+            matrix_report_id: matrixReportId,
+          },
+        });
+      }
+
+      return ratingRecord;
     },
   },
 };
---
+++ src/types/customTypes.ts
@@ -0,0 +1,9 @@
+type ReportInput = {
+  matrix_report_id: string;
+  competency_id: string;
+  user_id: string;
+  rating: number;
+  notes: string;
+};
+
+export { ReportInput };
